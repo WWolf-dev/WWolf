51becf27c684680d7470f618d821ae5862de3646
Lycoris-Dev <140905559+WWolf-dev@users.noreply.github.com>
2023-09-11 16:13:11 +0200

    Adding vehicle Return and re do logic on "VehicleMenu"

--- resources/[Server]/[Creation]/[Script]/ww-vehicleRent/client/main.lua
+++ resources/[Server]/[Creation]/[Script]/ww-vehicleRent/client/main.lua
@@ -28,7 +28,7 @@ if FrameworkUse == "ESX" then     --𝔽ℝ𝔸𝕄𝔼𝕎𝕆ℝ𝕂 𝕋𝕐�
         if UseTarget == "ox_target" then
             AccessNPC()
         end
-        VehicleAvailability()
+        initializeVehicleAvailability()
     end)
 
     function Blips()
@@ -93,6 +93,14 @@ if FrameworkUse == "ESX" then     --𝔽ℝ𝔸𝕄𝔼𝕎𝕆ℝ𝕂 𝕋𝕐�
                         end
                     },
                     {
+                        title = "Rendre un véhicule",
+                        description = "Rendre un véhicule une fois la location finit",
+                        icon = "fa-solid fa-card",
+                        onSelect = function()
+                            ReturnVehicleMenu()
+                        end
+                    },
+                    {
                         title = "Réserver un véhicule",
                         description = "Reservez un véhicule à l'avance",
                         icon = "fa-solid fa-card",
@@ -154,11 +162,33 @@ if FrameworkUse == "ESX" then     --𝔽ℝ𝔸𝕄𝔼𝕎𝕆ℝ𝕂 𝕋𝕐�
         lib.showContext("category_menu_npc_rental")
     end
     
+    -- == VARIABLES GLOBALES ==
     local available = {}
     local maintenance = {}
     local rented = {}
+    local rentedVehicles = {}
 
-    function VehicleAvailability()
+    -- == FONCTIONS UTILITAIRES ==
+    local function hexToRGB(hex)
+        hex = hex:gsub("#", "")
+        return {
+            tonumber("0x" .. hex:sub(1, 2)),
+            tonumber("0x" .. hex:sub(3, 4)),
+            tonumber("0x" .. hex:sub(5, 6))
+        }
+    end
+
+    local function distance2D(vec1, vec2)
+        return math.sqrt((vec2.x - vec1.x) ^ 2 + (vec2.y - vec1.y) ^ 2)
+    end
+
+    local function isVehicleNearCoords(coords, radius)
+        local vehicles = ESX.Game.GetVehiclesInArea(coords, radius)
+        return #vehicles > 0
+    end
+
+    -- == FONCTIONS PRINCIPALES ==
+    function initializeVehicleAvailability()
         for _, data in pairs(VehicleCategories) do
             for _, vehicleData in pairs(data.vehicle) do
                 if not available[vehicleData.name] then
@@ -168,24 +198,10 @@ if FrameworkUse == "ESX" then     --𝔽ℝ𝔸𝕄𝔼𝕎𝕆ℝ𝕂 𝕋𝕐�
         end
     end
 
-    function hexToRGB(hex)
-        hex = hex:gsub("#","")
-        return {tonumber("0x"..hex:sub(1,2)), tonumber("0x"..hex:sub(3,4)), tonumber("0x"..hex:sub(5,6))}
-    end
-
-    function distance2D(vec1, vec2)
-        return math.sqrt((vec2.x - vec1.x) ^ 2 + (vec2.y - vec1.y) ^ 2)
-    end
-
-    function isVehicleNearCoords(coords, radius)
-        local vehicles = ESX.Game.GetVehiclesInArea(coords, radius)
-        return #vehicles > 0
-    end
-
-    function getAvailableRentLocation(maxRadius)
+    local function getAvailableRentLocation(maxRadius)
         local playerCoords = GetEntityCoords(PlayerPedId())
         for radius = 5, maxRadius, 5 do
-            Citizen.Wait(200) -- Wait for 200ms
+            Citizen.Wait(200)
             for locationName, locationData in pairs(RentingNPC) do
                 for _, vehicleData in pairs(locationData.Vehicle) do
                     if distance2D(vehicleData.VehicleSpawnPoint, playerCoords) < radius and not isVehicleNearCoords(vehicleData.VehicleSpawnPoint, 5) then
@@ -196,8 +212,6 @@ if FrameworkUse == "ESX" then     --𝔽ℝ𝔸𝕄𝔼𝕎𝕆ℝ𝕂 𝕋𝕐�
         end
         return nil
     end
-
-    local rentedVehicles = {}
 
     function VehicleMenu(categoryName, vehicles)
         local vehicleOptions = {}
@@ -214,9 +228,6 @@ if FrameworkUse == "ESX" then     --𝔽ℝ𝔸𝕄𝔼𝕎𝕆ℝ𝕂 𝕋𝕐�
                 onSelect = function()
                     print("Vous avez choisi le véhicule: " .. vehicleData.label)
                     if available[vehicleData.name] and available[vehicleData.name] > 0 then
-                        -- Si le véhicule est disponible, nous le louons.
-                        available[vehicleData.name] = available[vehicleData.name] - 1
-                        rented[vehicleData.name] = (rented[vehicleData.name] or 0) + 1
                         local colorInput = lib.inputDialog('Configuration de Votre Véhicule', {
                             {
                                 type = 'color', 
@@ -232,9 +243,13 @@ if FrameworkUse == "ESX" then     --𝔽ℝ𝔸𝕄𝔼𝕎𝕆ℝ𝕂 𝕋𝕐�
                             }
                         })
                         if colorInput then
+                            -- Update the available and rented lists now
+                            available[vehicleData.name] = available[vehicleData.name] - 1
+                            rented[vehicleData.name] = (rented[vehicleData.name] or 0) + 1
+    
                             local primaryColor = hexToRGB(colorInput[1]) -- RGB values for primary color
                             local secondaryColor = hexToRGB(colorInput[2]) -- RGB values for secondary color
-                    
+    
                             local rentLocation = getAvailableRentLocation(30) -- Maximum radius of 30 units
                             if rentLocation then
                                 ESX.Game.SpawnVehicle(vehicleData.name, rentLocation.VehicleSpawnPoint, rentLocation.VehicleHeading, function(spawnedVehicle)
@@ -243,20 +258,26 @@ if FrameworkUse == "ESX" then     --𝔽ℝ𝔸𝕄𝔼𝕎𝕆ℝ𝕂 𝕋𝕐�
     
                                     TaskWarpPedIntoVehicle(PlayerPedId(), spawnedVehicle, -1)
                                     local plateNumber = GetVehicleNumberPlateText(spawnedVehicle) -- Get the vehicle's number plate
-                                    table.insert(rentedVehicles, plateNumber) -- Add the number plate to the rentedVehicles table
+    
+                                    -- Insert the number plate and player ID to the rentedVehicles table
+                                    table.insert(rentedVehicles, {plate = plateNumber, playerId = ESX.GetPlayerData().identifier})
+    
                                     print(json.encode(rentedVehicles))
                                 end)
                             else
                                 print("Désolé, aucun emplacement de location n'est disponible en ce moment.")
                                 return
                             end
-                        end
     
-                        print("Vous avez loué le véhicule: " .. vehicleData.label)
-                        print("Il reste " .. available[vehicleData.name] .. " exemplaire(s) de ce véhicule.")
-                        print("La catégorie 'available' contient " .. json.encode(available))
-                        print("La catégorie 'maintenance' contient " .. json.encode(maintenance))
-                        print("La catégorie 'rented' contient " .. json.encode(rented))
+                            print("Vous avez loué le véhicule: " .. vehicleData.label)
+                            print("Il reste " .. available[vehicleData.name] .. " exemplaire(s) de ce véhicule.")
+                            print("La catégorie 'available' contient " .. json.encode(available))
+                            print("La catégorie 'maintenance' contient " .. json.encode(maintenance))
+                            print("La catégorie 'rented' contient " .. json.encode(rented))
+                        else
+                            -- Si le joueur annule la sélection des couleurs, les compteurs ne sont pas touchés.
+                            print("Sélection des couleurs annulée.")
+                        end
                     else
                         -- Si le véhicule n'est pas disponible, nous informons le joueur.
                         print("Désolé, ce véhicule n'est pas disponible pour le moment.")
@@ -274,122 +295,62 @@ if FrameworkUse == "ESX" then     --𝔽ℝ𝔸𝕄𝔼𝕎𝕆ℝ𝕂 𝕋𝕐�
     
         lib.showContext("vehicle_menu_" .. categoryName)
     end
+
+
+    function ReturnVehicleMenu()
+        local playerIdentifier = ESX.GetPlayerData().identifier
+        local vehicleFound = false
     
+        local playerPed = PlayerPedId()
+        local playerCoords = GetEntityCoords(playerPed)
+        
+        -- Obtenir tous les véhicules dans un rayon de 30 unités
+        local vehiclesNearby = ESX.Game.GetVehiclesInArea(playerCoords, 30.0)
+    
+        -- Parcourir tous les véhicules à proximité
+        for _, vehicle in ipairs(vehiclesNearby) do
+            local plateNumber = GetVehicleNumberPlateText(vehicle)
+    
+            -- Vérifier si la plaque d'immatriculation du véhicule est dans la table rentedVehicles et appartient au joueur
+            for index, data in ipairs(rentedVehicles) do
+                if data.plate == plateNumber and data.playerId == playerIdentifier then
+                    -- Rendre le véhicule (détruire/retirer et mettre à jour les compteurs)
+                    ESX.Game.DeleteVehicle(vehicle)
+    
+                    -- Update the available and rented lists
+                    for name, count in pairs(rented) do
+                        if count > 0 then
+                            rented[name] = rented[name] - 1
+                            available[name] = (available[name] or 0) + 1
+                            break
+                        end
 
-
-    -- function MenuRental()
-    --     lib.registerContext({
-    --         id = "main_menu_npc_rental",
-    --         title = "Location de Véhicule",
-    --         options = {
-    --             {
-    --                 title = "Louer un véhicule",
-    --                 description = "Choisissez parmi une large liste de véhicule",
-    --                 icon = "fa-solid fa-card",
-    --                 onSelect = function()
-    --                     lib.showContext("category_menu_npc_rental")
-    --                 end
-    --             },
-    --             {
-    --                 title = "Réserver un véhicule",
-    --                 description = "Reservez un véhicule à l'avance",
-    --                 icon = "fa-solid fa-card",
-    --                 onSelect = function()
-    --                     print("yey")
-    --                 end
-    --             },
-    --             {
-    --                 title = "Historique de location",
-    --                 description = "Regardez les différents véhicule que vous avez loué",
-    --                 icon = "fa-solid fa-card",
-    --                 onSelect = function()
-    --                     print("yey")
-    --                 end
-    --             },
-    --             {
-    --                 title = "Point de fidélité",
-    --                 description = "Regardez combien de points vous avez gagné",
-    --                 icon = "fa-solid fa-card",
-    --                 onSelect = function()
-    --                     print("yey")
-    --                 end
-    --             },
-    --             {
-    --                 title = "Informations",
-    --                 description = "Consultez les informations sur les locations",
-    --                 icon = "fa-solid fa-card",
-    --                 onSelect = function()
-    --                     print("yey")
-    --                 end
-    --             },
-    --         }
-    --     })
-
-    --     lib.registerContext({
-    --         id = "rent_menu_vehicle_rental",
-    --         title = "Rent Vehicle",
-    --         options = {
-    --             {
-    --                 title = "Categorie",
-    --                 description = "Choisissez une catégorie de véhicule",
-    --                 icon = "fa-solid fa-card",
-    --                 onSelect = function()
-    --                     lib.showContext("category_menu_vehicle_rental")
-    --                 end
-    --             }
-    --         }
-    --     })
-
-    --     -- Générer les options dynamiquement à partir de RentingVehicle
-    --     local generatedOptions = {}
-    --     for category, vehicles in pairs(RentingVehicle) do
-    --         -- Crée un sous-menu pour chaque catégorie
-    --         local subMenuOptions = {}
-    --         for _, vehicle in ipairs(vehicles) do
-    --             table.insert(subMenuOptions, {
-    --                 title = vehicle.Name,
-    --                 description = "Louer " .. vehicle.Name,
-    --                 icon = "fa-solid fa-car",
-    --                 onSelect = function()
-    --                     print("You've chosen vehicle: " .. vehicle.Name)
-    --                     lib.inputDialog("Color Customization", {
-    --                         {type = 'color', label = 'Choose a color', default = '#ffffff'},
-    --                     })
-    --                     -- Votre logique pour louer le véhicule peut aller ici
-    --                 end
-    --             })
-    --         end
-
-    --         -- Enregistrer le sous-menu pour cette catégorie
-    --         local subMenuId = "vehicle_list_" .. category
-    --         lib.registerContext({
-    --             id = subMenuId,
-    --             title = "Véhicules - " .. category,
-    --             menu = "category_menu_vehicle_rental",
-    --             options = subMenuOptions
-    --         })
-
-    --         -- Ajouter une option pour la catégorie principale qui dirige vers ce sous-menu
-    --         table.insert(generatedOptions, {
-    --             title = category,
-    --             description = "Choisissez des véhicules dans la catégories " .. category,
-    --             icon = "fa-solid fa-card",
-    --             onSelect = function()
-    --                 print("You've chosen category : " .. category)
-    --                 lib.showContext(subMenuId)  -- Afficher le sous-menu
-    --             end
-    --         })
-    --     end
-
-    --     lib.registerContext({
-    --         id = "category_menu_vehicle_rental",
-    --         title = "Vehicle Category",
-    --         options = generatedOptions
-    --     })
-
-    --     lib.showContext("main_menu_vehicle_rental")
-    -- end
-
+                        if rented[name] == 0 then
+                            rented[name] = nil
+                        end
+                        
+                    end
+    
+                    table.remove(rentedVehicles, index)
+                    print("Vous avez rendu le véhicule avec la plaque: " .. plateNumber)
+                    print("La catégorie 'available' contient " .. json.encode(available))
+                    print("La catégorie 'maintenance' contient " .. json.encode(maintenance))
+                    print("La catégorie 'rented' contient " .. json.encode(rented))
+                    print(json.encode(rentedVehicles))
+                    vehicleFound = true
+                    break
+                end
+            end
+    
+            if vehicleFound then
+                break
+            end
+        end
+    
+        if not vehicleFound then
+            print("Aucun véhicule loué trouvé à proximité.")
+        end
+    end
 
 
 
